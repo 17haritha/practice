d3bdfd6401a7374a0a826cdf57831f48a3f956e0
17haritha <142497748+17haritha@users.noreply.github.com>
2023-08-27 16:16:28 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,14 @@
+public class odd {
+
+	public static void main(String[] args) {
+		System.out.println("odd number");
+		for(int i=0;i<=19;i++)
+			if(i%2!=0)
+			{
+			
+				System.out.print(i+" ");
+			}
+
+	}
+
+}
